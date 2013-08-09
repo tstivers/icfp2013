454595c595942cacfa46bd6625e632f5ced46d98
Tom Stivers <tstivers@gmail.com>
2013-08-08 19:59:08 -0500

    Can now get the list of problems.

---
+++ tomstuff/src/Game/GameConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Shared.ViewModels;
+
+namespace GameConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new GameClient.GameClient("0236vFGLj5xt5pYPigGQyveWYTGMysQaU4Ot3siN");
+
+            IEnumerable<Problem> problems = client.GetProblems();
+
+            Debugger.Break();
+        }
+    }
+}
---
+++ tomstuff/src/Game/Shared/ViewModels/Problem.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.ViewModels
+{
+    public class Problem
+    {
+        public string Id { get; set; }
+        public long Size { get; set; }
+        public string[] Operators { get; set; }
+        public bool Solved { get; set; }
+        public long TimeLeft { get; set; }
+    }
+}
